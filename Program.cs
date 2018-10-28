cbf8ba502bb388b86f536bebf511e4064d9cfc2c
lahuerta83 <Leigh.A.Huerta@live.mercer.edu>
2018-10-28 11:48:51 -0400

    Uploaded Unit Test files and correct some on files

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarkNPark
+{
+    public class Program
+    {
+
+        public static void Main (string [] args)
+        {
+            System system = new System();
+            Console.WriteLine("Welcome to Bark N Park Console Tester");
+            Console.WriteLine("Please enter a commad: a = checkin, b- extend time, c-dispense item, d- checkout, s- summary");
+            string s = "";
+            string name = "";
+            while(s != "exit")
+            {
+                s = Console.ReadLine();
+
+                switch (s.ToLower()[0]) {
+
+                    case 'a':
+                        Console.WriteLine("Enter Appointment Name: ");
+                         name = Console.ReadLine();
+                        Console.WriteLine(system.CheckIn(name, 60));
+                        if( system.getAppointmentByName(name) != null)
+                        {
+                            Console.WriteLine(system.getAppointmentByName(name).ToString());
+                        }
+                        
+                        break;
+                    case 'b':
+                        Console.WriteLine("Enter Appointment Name: ");
+                         name = Console.ReadLine();
+                        Console.WriteLine(system.requestextension(name, 30));
+                        if (system.getAppointmentByName(name) != null)
+                        {
+                            Console.WriteLine(system.getAppointmentByName(name).ToString());
+                        }
+                        
+                        break;
+                    case 'c':
+                        Console.WriteLine("Enter Appointment Name: ");
+                         name = Console.ReadLine();
+                        ItemType[] items = { ItemType.DOG_TREAT, ItemType.TOY };
+
+                        Console.WriteLine(system.addItem(name, items));
+
+                        break;
+                    case 'd':
+                        Console.WriteLine("Enter Appointment Name: ");
+                         name = Console.ReadLine();
+                        Console.WriteLine(system.Checkout(name));
+                        break;
+                    case 's':
+                        Console.WriteLine(system.ToString());
+
+                        break;
+
+                }
+
+               
+            }
+
+        }
+
+       
+    }
+
+ 
+}
